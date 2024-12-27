2f586d898d8d9b168baafea6ee5beead2f0d8018
pflanagan <pflanagan@grubhub.com>
2024-12-27 13:06:22 +0800

    use github sha instead of generic main and HEAD

--- src/test-end-of-line-comment.ts
+++ src/test-end-of-line-comment.ts
@@ -1,5 +1,5 @@
 
 // edit this file to see if the next line gets added to the PR body
-// edit: 11:54 PM Dec 26, 2024
+// edit: 1:02 PM Dec 28, 2024
 
 console.log('Comment at the end of a line') // MERGE: end of line comment, will include the whole line including code
