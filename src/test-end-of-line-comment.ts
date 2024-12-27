20aa0dbaded46bf512cf6693f42482c2f28d349d
pflanagan <pflanagan@grubhub.com>
2024-12-27 13:23:39 +0800

    better formatting

--- src/test-end-of-line-comment.ts
+++ src/test-end-of-line-comment.ts
@@ -2,4 +2,4 @@
 // edit this file to see if the next line gets added to the PR body
 // edit: 1:02 PM Dec 28, 2024
 
-console.log('Comment at the end of a line') // MERGE: end of line comment, will include the whole line including code
+console.log('Comment at the end of a line') // MERGE: we should remove all the code from the line and just print the comment
